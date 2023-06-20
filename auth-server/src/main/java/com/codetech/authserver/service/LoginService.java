c2f4fbe7ad84423f966c047474289330ec8269a4
khaouti nouhaila <khaoutinouhaila@gmail.com>
2023-06-20 10:57:26 +0100

    connecter keycloak avec postgresql

--- auth-server/src/main/java/com/codetech/authserver/service/LoginService.java
+++ auth-server/src/main/java/com/codetech/authserver/service/LoginService.java
@@ -22,30 +22,20 @@ public class LoginService {
 	@Autowired
 	RestTemplate restTemplate;
 
-	@Value("${spring.security.oauth2.client.registration.oauth2-client-credentials.client-id}")
-	private String clientId;
-
-	@Value("${spring.security.oauth2.client.registration.oauth2-client-credentials.client-secret}")
-	private String clientSecret;
-
-	@Value("${spring.security.oauth2.client.registration.oauth2-client-credentials.authorization-grant-type}")
-	private String grantType;
-
-
 	public ResponseEntity<LoginResponse> login(LoginRequest loginrequest) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.add("client_id", clientId);
-		map.add("client_secret", clientSecret);
-		map.add("grant_type", grantType);
+		map.add("client_id","auth-client" );
+		map.add("client_secret", "FqCNVMHcpNIrpix77bTFTRsph6vkSKsl");
+		map.add("grant_type", "password");
 		map.add("username", loginrequest.getUsername());
 		map.add("password", loginrequest.getPassword());
 
 		HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map,headers);
 
-		ResponseEntity<LoginResponse> response = restTemplate.postForEntity("http://localhost:8080/auth/realms/lbv-realm/protocol/openid-connect/token", httpEntity, LoginResponse.class);
+		ResponseEntity<LoginResponse> response = restTemplate.postForEntity("http://localhost:8080/auth/realms/lbv/protocol/openid-connect/token", httpEntity, LoginResponse.class);
 		return new ResponseEntity<>(response.getBody(),HttpStatus.OK);
 
 
